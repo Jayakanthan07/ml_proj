504256a68193513e2547498c98d28def78161a8c
Jayakanthan R <43201698+Jayakanthan07@users.noreply.github.com>
2020-05-28 14:48:49 +0530

    df

---
+++ docs/sketch.js
@@ -0,0 +1,59 @@
+//const URL = 'https://tamilcharpred.herokuapp.com/'
+const URL = 'https://cryptic-beach-64926.herokuapp.com/'
+//const URL = 'http://localhost:5000/'
+function setup() {
+  var canvas = createCanvas(356, 356);
+  background(255);
+  canvas.parent('drawingBoard');
+}
+
+function draw() {
+  stroke(0);
+  strokeWeight(6);
+  noFill();
+  if (mouseIsPressed === true) {
+    line(mouseX, mouseY, pmouseX, pmouseY);
+  }
+}
+
+$(document).ready(function () {
+  $("#predict").click(function () {
+    var button = $(this);
+    button.html("<i class=\"fas fa-spinner fa-spin\"></i> Predicting");
+    button.attr("disabled", true);
+
+    saveFrames('out', 'png', 1, 1, data => {
+      payload = JSON.stringify({
+        "img": data[0]['imageData']
+      });
+      $.ajax({
+        type: 'POST',
+        url: URL,
+        headers: {
+          'accept': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        },
+        data: payload,
+        success: function (data) {
+          vals = Object.values(data);
+
+          $("#predS").html(vals[0]);
+          $("#predH").show();
+          button.html("Predict");
+          button.attr("disabled", false);
+        },
+        error: function (e) {
+          button.html("Predict");
+          button.attr("disabled", false);
+        },
+        dataType: "json",
+        contentType: "application/json"
+      });
+    });
+  });
+
+  $("#reset").click(function () {
+    background(255);
+    $("#predH").hide();
+  });
+});
