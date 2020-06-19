fa5684a95778d0fd353a3188c560d9b255540ab9
Jayakanthan R <43201698+Jayakanthan07@users.noreply.github.com>
2020-06-19 15:43:12 +0530

    updated

--- docs/sketch.js
+++ docs/sketch.js
@@ -1,8 +1,8 @@
-const URL = 'https://tamilcharpred.herokuapp.com/'
-//const URL = 'https://cryptic-beach-64926.herokuapp.com/'
+//const URL = 'https://tamilcharpred.herokuapp.com/'
+const URL = 'https://cryptic-beach-64926.herokuapp.com/'
 //const URL = 'http://localhost:5000/'
 function setup() {
-  var canvas = createCanvas(356, 356);
+  var canvas = createCanvas(450, 380);
   background(255);
   canvas.parent('drawingBoard');
 }
@@ -14,6 +14,15 @@ function draw() {
   if (mouseIsPressed === true) {
     line(mouseX, mouseY, pmouseX, pmouseY);
   }
+}
+
+function typeWriteResult(){
+  $("#predH").show();
+  $("#predS").hide();
+  $("#predH div.result > p:first-child").addClass("type-write");
+  document.querySelector("#predH div.result > p:first-child").addEventListener("animationend",function(){
+    $("#predH div.result > p:last-child").show().addClass("type-write");
+  });
 }
 
 $(document).ready(function () {
@@ -36,12 +45,12 @@ $(document).ready(function () {
         data: payload,
         success: function (data) {
           vals = Object.values(data);
-
-          $("#predS").html(vals[0]);
-          $("#predH").show();
+         $("#predS").html(vals[0]);
+         $("#predH").show();
           button.html("Predict");
           button.attr("disabled", false);
-        },
+        
+      },
         error: function (e) {
           button.html("Predict");
           button.attr("disabled", false);
@@ -50,10 +59,12 @@ $(document).ready(function () {
         contentType: "application/json"
       });
     });
+    typeWriteResult(); // In this function I made predH show so let the above line 49 be as such
   });
 
   $("#reset").click(function () {
     background(255);
     $("#predH").hide();
+    $(".type-write").removeClass("type-write");
   });
 });
